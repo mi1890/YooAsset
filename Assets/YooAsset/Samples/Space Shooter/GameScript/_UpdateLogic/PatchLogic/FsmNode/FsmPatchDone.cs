71a8ed1d5466c60514e8fc93570139459e40c0aa
zhutao <tao.zhu@shadowmoon.com>
2023-01-31 11:16:50 +0800

    * [接入hybrid clr] 热更新跑通
    流程:1.接入package ,install 2.将代码分层 3. 设置hybrid setting ,热更的dll加入到列表中 4.设置工程il2cpp,关闭增量gc,打包一次 5.generateall 生成桥接文件,link.xml 6.打包热更后的代码

---
+++ Assets/YooAsset/Samples/Space Shooter/GameScript/_UpdateLogic/PatchLogic/FsmNode/FsmPatchDone.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniFramework.Machine;
+using UniFramework.Module;
+using System.Reflection;
+using System;
+using YooAsset;
+
+/// <summary>
+/// 流程更新完毕
+/// </summary>
+internal class FsmPatchDone : IStateNode
+{
+	void IStateNode.OnCreate(StateMachine machine)
+	{
+	}
+	void IStateNode.OnEnter()
+	{
+		PatchEventDefine.PatchStatesChange.SendEventMessage("开始游戏！");
+		//加载dll 执行gamestart
+#if UNITY_EDITOR
+        Assembly ass = Assembly.Load("SpaceShooterSample");
+#else
+		//从你的资源管理系统中获得热更新dll的数据
+		var handle = YooAssets.LoadAssetSync<TextAsset>("SpaceShooterSample.dll");
+		handle.WaitForAsyncComplete();
+        byte[] assemblyData = (handle.AssetObject as TextAsset).bytes;
+		Assembly ass = Assembly.Load(assemblyData);
+#endif
+
+        Type entryType = ass.GetType("GameManager");
+		Debug.Log($"entryType:{entryType}");
+        MethodInfo method = entryType.GetMethod("GameStart");
+        Action mainFunc = (Action)Delegate.CreateDelegate(typeof(Action), method);
+        mainFunc();
+
+    }
+	void IStateNode.OnUpdate()
+	{
+	}
+	void IStateNode.OnExit()
+	{
+	}
+}
